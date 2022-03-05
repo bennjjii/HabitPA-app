6f826267225960d309f0b7465db2064c121b85c7
Ben <ben@Bens-MacBook-Pro.local>
2022-03-05 01:01:02 +0000

    Init commit

---
+++ components/BananaShower.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {gql, useQuery} from '@apollo/client';
+import {Text} from 'react-native';
+
+const GET_BANANA = gql`
+  query GetBanana {
+    banana
+  }
+`;
+
+const BananaShower = () => {
+  const {loading, error, data} = useQuery(GET_BANANA);
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>{error.message}</Text>;
+  return <Text>{data?.banana}</Text>;
+};
+
+export default BananaShower;
